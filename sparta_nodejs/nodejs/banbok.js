09eec56d6ae5d7b4d17d8f0cf7d6180de5928ce9
Justin-SJ-Hong <41540100+Justin-SJ-Hong@users.noreply.github.com>
2022-08-17 13:50:19 +0900

    Create banbok.js

---
+++ sparta_nodejs/nodejs/banbok.js
@@ -0,0 +1,7 @@
+const example = ['apple', 'banana', 'mango', 'kiwi', 'melon', 'grape'];
+
+console.log(example.length);
+for (let index = 0; index < example.length; index++) {
+    const element = example[index];
+    console.log(element);
+}
